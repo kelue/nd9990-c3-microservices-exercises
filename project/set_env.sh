8ed49c826ea193170d2ad3e668a09b341954d287
kelue <austincmoon5@gmail.com>
2021-09-26 15:08:05 +0100

    refactored services into microservices

--- project/set_env.sh
+++ project/set_env.sh
@@ -1,11 +1,11 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
-export POSTGRES_USERNAME=postgres
-export POSTGRES_PASSWORD=password1
-export POSTGRES_HOST=udagram2.csixzufepzrt.us-west-2.rds.amazonaws.com
-export POSTGRES_DB=udagram
-export AWS_BUCKET=arn:aws:s3:::udaconnect
-export AWS_REGION=us-west-2
-export AWS_PROFILE=test
+export POSTGRES_USERNAME=monodbuser
+export POSTGRES_PASSWORD=PLSuMq59p9MK5z
+export POSTGRES_HOST=keluemonodb.cnl7egffq1t4.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=monodb
+export AWS_BUCKET=arn:aws:s3:::kelue-mono-bucket
+export AWS_REGION=us-east-1
+export AWS_PROFILE=default
 export JWT_SECRET=testing
 export URL=http://localhost:8100
